bde46d835acfa25dfbeb56921ab1c17967e96fcf
SoyJohanFer <83677959+SoyJohanFer@users.noreply.github.com>
2021-05-31 16:38:13 -0500

    Add files via upload

---
+++ JuntandoParejasV1.py
@@ -0,0 +1,74 @@
+import time
+import platform
+import os
+
+rojo = '\033[31m'
+normal = '\033[39m'
+
+print(normal+"")
+
+respuesta1 = "1"
+respuesta2 = "2"
+
+def limpiar_pantalla():
+    time.sleep(2)
+
+    if platform.system() == 'Windows':
+        os.system('cls')
+
+    else:
+        os.system('clear')
+
+
+if __name__ == '__main__':
+    limpiar_pantalla()
+
+time.sleep(1)
+
+print (rojo+"               ,;;;,   ,;;;,                             ")
+time.sleep(1)
+print (rojo+"              ;;;;;;;,;;;;;;;                            ")
+time.sleep(1)
+print (rojo+"     .:::.   .::::;;;;;;;;;;;                            ")
+time.sleep(1)
+print (rojo+"    :::::::.:::::::;;;;;;;;;'                            ")
+time.sleep(1)
+print (rojo+"    :::::::::::::::;;;;;;;'          by: Sherlock           ")
+time.sleep(1)
+print (rojo+"    ':::::::::::::';;;;;'                                ")
+time.sleep(1)
+print (rojo+"      ':::::::::'   ';'                                  ")
+time.sleep(1)
+print (rojo+"        ':::::'                                          ")
+time.sleep(1)
+print (rojo+"          ':'                                            ")
+time.sleep(1)
+
+
+print(normal+"")
+nombre = input("Cual es tu nombre: ")
+print(f"Hola {nombre}")
+
+print("Queria preguntarte si... ¿Querias ser mi novia? :3 ")
+print("1. Si <3")
+print("2. No :(")
+print("")
+respuesta = input()
+
+if (respuesta == respuesta1):
+ print("")
+ print("")
+
+ print(f"Sabia que si querias serlo, te amo {nombre}")
+ time.sleep(5)
+ exit
+
+
+
+if (respuesta == "2"):
+ print("")
+ print("")
+
+ print("Yo solo queria hacerte feliz, espero encuentres a alguien que te ame y te valore mucho :( ")
+ time.sleep(3)
+ exit
