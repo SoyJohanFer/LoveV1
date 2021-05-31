9815ae1571d3d0eb053c4741d63bf8670ce65014
SoyJohanFer <83677959+SoyJohanFer@users.noreply.github.com>
2021-05-31 16:57:30 -0500

    Update JuntandoParejasV1.py

--- JuntandoParejasV1.py
+++ JuntandoParejasV1.py
@@ -10,7 +10,18 @@ print(normal+"")
 respuesta1 = "1"
 respuesta2 = "2"
 
-clear
+def limpiar_pantalla():
+    time.sleep(2)
+
+    if platform.system() == 'Windows':
+        os.system('cls')
+
+    else:
+        os.system('clear')
+
+
+if __name__ == '__main__':
+    limpiar_pantalla()
 
 time.sleep(1)
 
